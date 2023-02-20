118613458bbf4377ac63cd6b710809a7ec73abc8
wonjjong <wonjjong.dev@gmail.com>
2023-02-20 23:56:51 +0900

    Dollar 부작용 해결

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,7 +7,9 @@ public class Main {
     @Test
     void textMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
         assertEquals(10, five.amount);
+        product = five.times(3);
+        assertEquals(15,product.amount);
     }
 }
