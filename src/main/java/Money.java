d428fec749948448d629c9bf7527bc2b4a331455
wonjjong <wonjjong.dev@gmail.com>
2023-02-23 00:09:27 +0900

    times 메소드 시그니처 통일 / 팩토리 메서드 도입

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,9 +1,19 @@
-public class Money {
+abstract class Money {
     protected int amount;
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
     }
+
+    abstract Money times(int multiplier);
 }
